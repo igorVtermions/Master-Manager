4b3f08718ae906dd2f5cd63143a86dd904213644
Igor Vinicius <igorviniciusf10@gmail.com>
2023-09-27 14:32:29 -0300

    coloquei o local storage

--- src/Context/CharContext.jsx
+++ src/Context/CharContext.jsx
@@ -1,51 +1,72 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+import useLocalStorage from "../CustomHook/useLocalStorage";
 
 export const CharContext = createContext({});
 
-export default function CharProvider({children}){
-    
-    const [addChar, setAddChar] = useState(false);
-    const [charName, setCharName] = useState();
-    const [imageUrl, setImageUrl] = useState();
-    const [charClass, setCharClass] = useState();
-    const [maxHP, setMaxHP] = useState();
-    const [bonus, setBonus] = useState('');
-    const [charBonus, setCharBonus] = useState([]);
-    const [character, setCharacter] = useState([]);
+export default function CharProvider({ children }) {
+  const [addChar, setAddChar] = useState(false);
+  const [charName, setCharName] = useState();
+  const [imageUrl, setImageUrl] = useState();
+  const [charClass, setCharClass] = useState();
+  const [maxHP, setMaxHP] = useState();
+  const [bonus, setBonus] = useState("");
+  const [charBonus, setCharBonus] = useState([]);
+  const [character, setCharacter] = useLocalStorage("characters", []);
 
-    const closeAddChar = () =>{
-        setAddChar(false)
-    }
+  const closeAddChar = () => {
+    setAddChar(false);
+  };
 
-    const addBonus = () =>{
-        setCharBonus((prev) => [...prev, bonus])
-        setBonus('');
-    }
+  const addBonus = () => {
+    setCharBonus((prev) => [...prev, bonus]);
+    setBonus("");
+  };
 
-    const removeBonus = (bonus) =>{
-        const filteredBonus = charBonus.filter((item) => item !== bonus);
-        setCharBonus(filteredBonus);
-    }
-    
-    const addCharacter = () =>{
-        const charInfo = {
-            name: charName,
-            image: imageUrl,
-            class: charClass,
-            hp: maxHP,
-            attributes: charBonus,
-        }
-        setCharacter((prev) => [...prev, charInfo])
-        setCharName('')
-        setImageUrl('')
-        setCharClass('')
-        setMaxHP('')
-        setCharBonus([])
-    }
+  const removeBonus = (bonus) => {
+    const filteredBonus = charBonus.filter((item) => item !== bonus);
+    setCharBonus(filteredBonus);
+  };
 
-    return(
-        <CharContext.Provider value={{addChar, setAddChar, closeAddChar, charName, setCharName, bonus, setBonus, charBonus, addBonus, removeBonus, imageUrl, setImageUrl, charClass, setCharClass, maxHP, setMaxHP, addCharacter, character}}>
-            {children}
-        </CharContext.Provider>
-    )
+  const addCharacter = () => {
+    const charInfo = {
+      name: charName,
+      image: imageUrl,
+      class: charClass,
+      hp: maxHP,
+      attributes: charBonus,
+    };
+    setCharacter((prev) => [...prev, charInfo]);
+    setCharName("");
+    setImageUrl("");
+    setCharClass("");
+    setMaxHP("");
+    setCharBonus([]);
+  };
+
+  return (
+    <CharContext.Provider
+      value={{
+        addChar,
+        setAddChar,
+        closeAddChar,
+        charName,
+        setCharName,
+        bonus,
+        setBonus,
+        charBonus,
+        addBonus,
+        removeBonus,
+        imageUrl,
+        setImageUrl,
+        charClass,
+        setCharClass,
+        maxHP,
+        setMaxHP,
+        addCharacter,
+        character,
+      }}
+    >
+      {children}
+    </CharContext.Provider>
+  );
 }
